b1883df437ba66c03f749db4560b66ed76167b1f
Saul Echeverri <111350593+saulolo@users.noreply.github.com>
2022-10-29 03:37:31 -0500

    Version 1. Definineto variables. 29-10-22

--- src/Reto1/Rifa.java
+++ src/Reto1/Rifa.java
@@ -6,8 +6,15 @@ public class Rifa {
 
     public static void main(String[] args) {
        
+        /*Usted y un grupo de amigos ha decidido realizar una rifa de 1'000.000
+        de pesos para recaudar fondos en pro de los habitantes de la calle del 
+        municipio en el que usted habita.*/
+        
         System.out.println("RIFA (RETO CICLO PYTHON)");
         System.out.println("------------------------");
+        
+        int n = 13;
+        int b = 30;
         
     }
 
