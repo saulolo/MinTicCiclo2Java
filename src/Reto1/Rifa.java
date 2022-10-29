4f53b9e94b3a88527e22a125d89d10f788a4fa6d
Saul Echeverri <111350593+saulolo@users.noreply.github.com>
2022-10-29 03:15:46 -0500

    Version 1. Reto 1. Empezando 29-10-22

---
+++ src/Reto1/Rifa.java
@@ -0,0 +1,14 @@
+package Reto1;
+
+/* @author: Saulolo */
+
+public class Rifa {
+
+    public static void main(String[] args) {
+       
+        System.out.println("RIFA (RETO CICLO PYTHON)");
+        System.out.println("------------------------");
+        
+    }
+
+}
