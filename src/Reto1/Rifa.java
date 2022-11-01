d212506be831cc5cec114d23bcb145cf6d2835e8
Saul Echeverri <111350593+saulolo@users.noreply.github.com>
2022-11-01 11:49:00 -0500

    Erercio de la rifa culminado. 01-11-22

--- src/Reto1/Rifa.java
+++ src/Reto1/Rifa.java
@@ -1,10 +1,14 @@
 package Reto1;
 
+import java.util.Scanner;
+
 /* @author: Saulolo */
 
 public class Rifa {
 
     public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
        
         /*Usted y un grupo de amigos ha decidido realizar una rifa de 1'000.000
         de pesos para recaudar fondos en pro de los habitantes de la calle del 
@@ -13,9 +17,29 @@ public class Rifa {
         System.out.println("RIFA (RETO CICLO PYTHON)");
         System.out.println("------------------------");
         
-        int n = 13;
+        int n = (int) ((Math.random() * 30) + 1);
         int b = 30;
+        int intento;
+        int acomulador = 0;
         
+        
+        while (true) {    
+            
+            System.out.println("Diga un número entre 0 y " + b);
+            intento = input.nextInt();
+            acomulador = acomulador + 1;
+            
+            if (intento > n) {
+                System.out.println("¡Ups! Te pasaste");
+            } else if (intento < n) {
+                System.out.println("¡Ups! Estas por debajo");
+            } else if (intento == n){
+                System.out.println("¡LO LOGRASTE! usaste " + acomulador + " intentos");
+                break;
+            }
+           
+        }
+
     }
 
 }
