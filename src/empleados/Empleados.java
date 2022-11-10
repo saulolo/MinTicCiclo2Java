c54f2f35089ea10b4ef6345f1e6c2047f121a5c8
Saul Echeverri <111350593+saulolo@users.noreply.github.com>
2022-11-10 15:35:24 -0500

    Clase 6. 10-11-22

---
+++ src/empleados/Empleados.java
@@ -0,0 +1,50 @@
+package empleados;
+
+import java.util.Scanner;
+
+/* @author: Saulolo */
+
+public class Empleados { 
+    
+    public static class empleado{ //Método constructor
+        private String nombre;
+        
+        
+        public empleado(String nombre){
+            this.nombre = nombre;
+            //System.out.println("Se construyo correctamente el empleado " + nombre);//se construye esta linea para verificar
+        }
+
+        public String getNombre() { //Método Get
+            return nombre;
+        }
+
+        public void setNombre(String nombre) { //Método Set
+            this.nombre = nombre;
+            System.out.println("El empleado ahora se llama " + nombre);
+        }
+        
+        //Método toString()
+        //devuelve un string formado por las palabras que queremos y el atributo consultado
+        @Override
+        public  String toString(){
+            return "El empleado se llama " + nombre;
+        }
+        
+        
+    
+        
+    } 
+
+    public static void main(String[] args) { //Método principal
+        Scanner input = new Scanner(System.in); //Creamos un objeto de tipo Scanner para hacer input
+        empleado no1 = new empleado("Roberto");//Creamos el objeto empleado
+        
+        System.out.println("Ingrese el nuevo nombre: "); //Mostrar mensaje en pantalla pidiendo el nombre del nuevo empleado
+        String nuevoNombre = input.nextLine(); //Nueva variable alimentada por el usuario
+        
+        no1.setNombre(nuevoNombre);
+        System.out.println(no1.toString());
+        
+    }
+}
