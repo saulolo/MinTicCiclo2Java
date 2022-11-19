9078361232b5a742ac96e341d29d2833a8875df8
Saul Echeverri <111350593+saulolo@users.noreply.github.com>
2022-11-18 22:32:44 -0500

    Clase 4 Java para dummies min 1:008:00. 18-11-22

---
+++ src/JavaParaDummies/SuperPersonaje.java
@@ -0,0 +1,46 @@
+package JavaParaDummies;
+
+/* @author: Saulolo */
+
+public class SuperPersonaje {
+    
+    //1.Atributos (Caracteristicas)
+    private String nombre; //Si no le pongo private, por defecto es publico
+    private String poder;  //private quiere decir que yo no pueda acceder a estos
+    private int edad;      // atributos, fuera de esta clase, casi siempre 
+    private double valorComercial;  //deberian estar en private
+    private boolean tieneAlas;
+    private char genero;
+    private String equipo;
+    private double nivelDePoder;
+    private int vidas;
+    
+    //3. Constructor: generar un objeto e inizializarlo
+    
+    //2.Metodos (acciones)
+    public void correr(){
+        
+    }
+    
+    public void atacar(){
+        
+    }
+    
+    public void saltar(){
+        
+    }
+    
+    public void defender(){
+        
+    }
+    
+    public void volar(){
+        
+    }
+    
+    public void jugar(){
+        
+    }
+    
+
+}
---
+++ src/JavaParaDummies/main.java
@@ -0,0 +1,28 @@
+package JavaParaDummies;
+
+/* @author: Saulolo */
+
+public class main {
+    
+    public static void main(String[] args) {
+        //3.Creabdo Objetos
+        
+        SuperPersonaje miPersonaje = new SuperPersonaje();
+        
+        System.out.println(miPersonaje);
+        System.out.println(miPersonaje.edad);
+        System.out.println(miPersonaje.);
+        
+        miPersonaje. = 30;
+        miPersonaje.valorComercial = -85;
+        
+        System.out.println(miPersonaje.edad);
+        System.out.println(miPersonaje.valorComercial);
+        
+        
+        //  QUEDE EN 1:08;00
+        
+        
+    }
+
+}
